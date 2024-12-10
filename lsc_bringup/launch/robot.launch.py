5dd1e8e5fc6c8056ed88dec67dbe3f35f73d5ebf
harleylara <harley.lara@outlook.com>
2024-12-10 11:53:41 +0100

    update

--- lsc_bringup/launch/robot.launch.py
+++ lsc_bringup/launch/robot.launch.py
@@ -3,15 +3,15 @@ from launch import LaunchDescription, LaunchDescriptionEntity
 from launch.actions import IncludeLaunchDescription, OpaqueFunction, DeclareLaunchArgument
 from launch.substitutions import EnvironmentVariable, PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 
 def launch_args(context) -> list[LaunchDescriptionEntity]:
 
     declared_args = []
 
     declared_args.append(DeclareLaunchArgument(
-        "namespace",
-        default_value=EnvironmentVariable("ROBOT_NAMESPACE", default_value=""),
+        "robot_name",
+        default_value=EnvironmentVariable("ROBOT_NAME", default_value="robot"),
         description="Namespace for all topics and tfs",
     ))
 
@@ -25,18 +25,21 @@ def launch_args(context) -> list[LaunchDescriptionEntity]:
 
 def launch_setup(context) -> list[LaunchDescriptionEntity]:
 
+    controllers_path_performed = PathJoinSubstitution([
+        FindPackageShare("lsc_bringup"),
+        "config",
+        "controllers.yml"
+    ]).perform(context)
+
     robot_description_content = xacro.process_file(
         PathJoinSubstitution([
-            FindPackageShare("rosbot_description"),
+            FindPackageShare("lsc_description"),
             "urdf",
             "rosbot.urdf.xacro"
         ]).perform(context),
         mappings={
-                "use_sim": LaunchConfiguration("use_sim").perform(context),
-                "use_gpu": "false",
-                "mecanum": "false",
-                "simulation_engine": "gazebo-classic",
-                "namespace": LaunchConfiguration("namespace").perform(context)
+            "robot_name": LaunchConfiguration("robot_name").perform(context),
+            "controllers": controllers_path_performed,
         }
     ).toxml()
 
@@ -68,13 +71,24 @@ def launch_setup(context) -> list[LaunchDescriptionEntity]:
             "-x", "0",
             "-y", "0",
             "-z", "0.4",
-            "-entity", "rosbot"]
+            "-entity", LaunchConfiguration("robot_name")]
+    )
+
+    controller_manager_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "-p", controllers_path_performed,
+            "joint_state_broadcaster",
+        ],
     )
 
     return [
+        PushRosNamespace(LaunchConfiguration("robot_name")), # this help to add robot_name to all entities
         robot_state_publisher,
         spawn_robot,
-        launch_gazebo
+        launch_gazebo,
+        controller_manager_spawner
     ]
 
 def generate_launch_description() -> LaunchDescription:
